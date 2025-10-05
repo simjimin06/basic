01492ab13e7cd2e0a25b58b8e25422d551aa4022
simjimin <simjimin@gm.gist.ac.kr>
2025-10-06 01:49:59 +0900

    Define PostModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,2 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './posts.controller'
+import { PostService } from './posts.service';
+
+@Module({
+    controllers: [PostController],
+    providers: [PostService],
+    exports:[PostService]
+
+})
+
+export class PostModule{}
