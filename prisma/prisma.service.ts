cbc1526e296dde257712d3391168652be4a099bd
simjimin <simjimin@gm.gist.ac.kr>
2025-10-12 22:31:13 +0900

    console.log에서 logger로 변경

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,11 +1,13 @@
 // src/prisma/prisma.service.ts
 
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 // PrismaClient를 상속받아 NestJS의 라이프사이클에 연결.
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name); 
+
   constructor() {
     // DB 연결 로그를 활성화하여 터미널에서 쿼리를 확인가능.
     super({
@@ -16,13 +18,13 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   // NestJS 모듈이 초기화될 때 DB에 연결!!!!
   async onModuleInit() {
     await this.$connect();
-    console.log('PrismaService: Database connected successfully.');
+    this.logger.log('PrismaService: Database connected successfully.');
   }
 
   // NestJS 모듈이 종료될 때 DB 연결을 끊음.
   async onModuleDestroy() {
     await this.$disconnect();
-    console.log('PrismaService: Database disconnected.');
+    this.logger.log('PrismaService: Database disconnected.');
   }
 }
 
